ae6a56c5330382fc42ef922270ff3a9751527043
AnushaChattopadhyayHeidelberg <anusha.chatto@stud.uni-heidelberg.de>
2022-12-12 06:10:41 +0100

    test:import into main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #load test data
-import pandas as pd
-import numpy as np
-x_test= pd.read_csv("../data/x_test_pf4T2aK.csv")
-x_train= pd.read_csv("../data/x_train_ACFqOMF.csv")
-y_train= pd.read_csv("../data/y_train_HNMbC27.csv")
+#import pandas as pd
+#import numpy as np
+#x_test= pd.read_csv("../data/x_test_pf4T2aK.csv")
+#x_train= pd.read_csv("../data/x_train_ACFqOMF.csv")
+#y_train= pd.read_csv("../data/y_train_HNMbC27.csv")
+
+import simplemovingaverage
