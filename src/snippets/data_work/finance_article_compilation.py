22fbe53dbcb18c292a628229c12c99c415acf365
Anusha Chattopadhyay <anusha.chattopadhyay@stud.uni-heidelberg.de>
2023-02-03 21:03:14 +0100

    test: compilation script date script fix

--- src/snippets/data_work/finance_article_compilation.py
+++ src/snippets/data_work/finance_article_compilation.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
+from datetime import datetime
+from datetime import timedelta
 df_market = pd.read_csv('completed_vix_archive_formatted.csv')
 directory = "./bis_speeches_tables_2/"
 data = {
@@ -37,158 +39,68 @@ for filename in os.listdir(directory):
 stored_df["Diff_VIX_1d"] = stored_df["CLOSE"]
 #print("1d values added")
     # df_1w = np.where(stored_df['DATE'] == (df_market['DATE']))
-'''
+
 
 for date in stored_df["DATE"]:
     date_list = str(date).split('-')
     date_list = list(map(int, date_list))
     print(date_list)
-    one_week_date = date_list.copy()
-    two_week_date = date_list.copy()
-    if (date_list[1] == 2):
-        if (date_list[0] % 4 == 0 and date_list[0] % 100 != 4):
-            if (date_list[2] < 23):
-                    if(date_list[2] < 16):
-                        one_week_date[2] = date_list[2]+7
-                        two_week_date[2] = date_list[2]+14
-                    else:
-                        one_week_date[2] = date_list[2]+7
-                        temp = date_list[2]+14
-                        temp = temp-29
-                        two_week_date[2] = temp
-                
-                        two_week_date[1] = date_list[1] + 1
-                        
-            else:
-                    temp = date_list[2]+7
-                    temp = temp-29
-                    temp2 = date_list[2]+14
-                    temp2 = temp2-29
-                    one_week_date[2] = temp
-                    two_week_date[2] = temp2
-
-                    one_week_date[1] = date_list[1] + 1
-                    two_week_date[1] = date_list[1] + 1
-
-                
-        else:
-                if (date_list[2] < 22):
-                    one_week_date[2] = date_list[2]+7
-                    if(date_list[2] < 15):
-                        one_week_date[2] = date_list[2]+7
-                        two_week_date[2] = date_list[2]+14
-                    else:
-                        one_week_date[2] = date_list[2]+7
-                        temp = date_list[2]+14
-                        temp = temp-28
-                        two_week_date[2] = temp
-
-                        two_week_date[1] = date_list[1] + 1
-
-                else:
-                    temp = date_list[2]+7
-                    temp = temp-29
-                    temp2 = date_list[2]+14
-                    temp2 = temp2-29
-                    one_week_date[2] = temp
-                    two_week_date[2] = temp2
-     
-                    one_week_date[1] = date_list[1] + 1
-                    two_week_date[1] = date_list[1] + 1
-                 
-    elif (date_list[1] == 4 or date_list[1] == 6 or date_list[1] == 9 or date_list[1] == 11):
-        if (date_list[2] < 24):
-            if(date_list[2] < 17):
-                        one_week_date[2] = date_list[2]+7
-                        two_week_date[2] = date_list[2]+14
-            else:
-                        one_week_date[2] = date_list[2]+7
-                        temp = date_list[2]+14
-                        temp = temp-30
-                        two_week_date[2] = temp
-                           
-                        two_week_date[1] = date_list[1] + 1
-                    
-        else:
-                    temp = date_list[2]+7
-                    temp = temp-30
-                    temp2 = date_list[2]+14
-                    temp2 = temp2-30
-                    one_week_date[2] = temp
-                    two_week_date[2] = temp2
-                    
-                    one_week_date[1] = date_list[1] + 1
-                    two_week_date[1] = date_list[1] + 1
-                    
-    elif (date_list[1] == 1 or date_list[1] == 3 or date_list[1] == 5 or date_list[1] == 7 or date_list[1] == 8 or date_list[1] == 10 or date_list == 12):
-        if (date_list[2] < 25):
-            if(date_list[2] < 18):
-                        one_week_date[2] = date_list[2]+7
-                        two_week_date[2] = date_list[2]+14
-            else:
-                        one_week_date[2] = date_list[2]+7
-                        temp = date_list[2]+14
-                        temp = temp-31
-                        two_week_date[2] = temp
-                        if (date_list[1] != 12):
-                            
-                            two_week_date[1] = date_list[1] + 1
-                        else:
-                            two_week_date[1] = 1
-                            two_week_date[0] = date_list[0] + 1
-        else:
-                    temp = date_list[2]+7
-                    temp = temp-31
-                    temp2 = date_list[2]+14
-                    temp2 = temp2-31
-                    one_week_date[2] = temp
-                    two_week_date[2] = temp2
-                    if (date_list[1] != 12):
-                        one_week_date[1] = date_list[1] + 1
-                        two_week_date[1] = date_list[1] + 1
-                    else:
-                        one_week_date[1] = 1
-                        one_week_date[0] = date_list[0] + 1
-                        two_week_date[1] = 1
-                        two_week_date[0] = date_list[0] + 1
-   
-
-    one_week_date_join = str(
-        one_week_date[0]) +'-'+ str(one_week_date[1]) +'-'+ str(one_week_date[2])
+    one_w = timedelta(weeks = 1)
+    two_w = timedelta(weeks = 2)
+    #one_week_date = date_list.copy()
+    #two_week_date = date_list.copy()
+    current_date_obj = datetime(date_list[0], date_list[1], date_list[2], 00, 00)
+    one_week_obj = current_date_obj + one_w
+    two_week_obj = current_date_obj + two_w
+    one_week_date = [one_week_obj.year, one_week_obj.month , one_week_obj.day]
     print(one_week_date)
-
-    two_week_date_join = str(
-        two_week_date[0]) +'-'+ str(two_week_date[1]) +'-'+ str(two_week_date[2])
+    date1= "-".join(str(x) for x in one_week_date)
+    two_week_date = [two_week_obj.year, two_week_obj.month , two_week_obj.day]
     print(two_week_date)
-
-    one_two_week_date_join_df = {
-        "date1w" : one_week_date_join,
-        "date2w" : two_week_date_join
+    date2="-".join(str(x) for x in two_week_date)
+    
+    tempdf_date = {
+    "date1w": [date1],
+    
+    "date2w": [date2]
+    
     }
-    df1_2w= df1_2w.append(one_two_week_date_join_df, ignore_index = True)
+    tempdf = pd.DataFrame(tempdf_date)
+    df1_2w = df1_2w.append(tempdf)
 
 
-
+df1_2w = df1_2w.reset_index(drop=True)
 
 print("oneweeklist and twoweek created")
 #placeholder = date1_2w.copy()
-df_market = df_market.rename(columns={"DATE":"date1w","CLOSE":"Diff_VIX_1w"})
+#df_market = df_market.rename(columns={"DATE":"date1w","CLOSE":"Diff_VIX_1w"})
 #df1_2w = df1_2w.fillna(df1_2w[["CLOSE"]].merge(df_market, on="DATE", how='left'))
 #df1_2w.update(df1_2w[['DATE', 'CLOSE']].merge(df_market, 'left'))
 #df1_2w = df1_2w.update(df_market)
 #df1_2w['Diff_VIX_1w'] = df_market['CLOSE']
 #df1_2w = df1_2w.join(df_market, on="DATE")
 #df1_2w["Diff_VIX_1w"] = df_market.where(df1_2w["DATE"] == df_market["DATE"])
-df1_2w = df1_2w.merge(df_market, on='date1w')
-#print(df1_2w)
-'''
+#df1_2w = df1_2w.merge(df_market, on='date1w')
+#print(df1_2w.head(5))
 
-'''
-for index in df1_2w.index:
-    stored_df.at[index,'Diff_VIX_1w'] = stored_df.query("DATE=='" + df1_2w.at[index,'date1w'] + "'" )["OPEN"]
-    stored_df.at[index,'Diff_VIX_2w'] = stored_df.query("DATE=='" + df1_2w.at[index,'date2w'] + "'" )["OPEN"]
-'''
-
+print(df1_2w.head(5))
 print(stored_df.head(5))
-stored_df.to_csv("merged_finance.csv", mode='a', index=False, header=True)
+
+for index in df1_2w.index:
+ 
+    #print(str(df1_2w.at[1,'date2w']))
+    ex1= "DATE=='" + str(df1_2w.at[index,'date1w']) + "'"
+    ex2 = "DATE=='" + str(df1_2w.at[index,'date2w']) + "'" 
+    stored_df.at[index,'Diff_VIX_1w'] = stored_df.eval(ex1 )['OPEN']
+    stored_df.at[index,'Diff_VIX_2w'] = stored_df.eval(ex2 )['OPEN']
+
+
+
+'''
+now = datetime.now()
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+'''
+print(stored_df.head(5))
+#stored_df.to_csv("merged_finance"+date_time+".csv", mode='a', index=False, header=True)
 
