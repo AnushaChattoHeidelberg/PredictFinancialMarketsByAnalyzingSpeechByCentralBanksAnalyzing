b150e2741fb257678737f49facd43c52c3a75e10
Anusha Chattopadhyay <anusha.chattopadhyay@stud.uni-heidelberg.de>
2023-02-24 06:55:34 +0100

    code:upzip.py file list updated

--- upzip.py
+++ upzip.py
@@ -3,7 +3,7 @@
 import zipfile
 
 #add your zip file in the list, please also add the pathname example: data/autoupzippeddata/filename.csv in the gitignore
-list_of_files_in_data = ["bis_speeches_tables.zip","merged_finance_1w_2w_split01.zip","merged_finance_1w_2w_split02.zip","merged_finance_1w_2w_split03.zip", "merged_finance_1w_2w_split04.zip", "merged_finance_1w_2w.zip"]
+list_of_files_in_data = ["bis_speeches_tables.zip","merged_finance_1w_2w_split01.zip","merged_finance_1w_2w_split02.zip","merged_finance_1w_2w_split03.zip", "merged_finance_1w_2w_split04.zip", "merged_finance.zip"]
 
 for name in list_of_files_in_data:
     with zipfile.ZipFile("./data/"+name,"r") as zip_ref:
